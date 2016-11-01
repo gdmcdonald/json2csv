a7e9f7575c4d368294d2124de5be8ff972fb01ab
gdmcdonald <gordon27@gmail.com>
2016-11-02 10:47:32 +1100

    Update json2csv.py
    Comments and better named variables.

--- json2csv.py
+++ json2csv.py
@@ -1,33 +1,34 @@
-import json
 import csv
 import gzip
 import time
 
-#input .json.gz file
-inputFileName='aggressive_dedup.json.gz'
+#input .json.gz file, in the same folder as this python script.
+inputFileName='reviews_Electronics_5.json.gz'
 
-#remove last 8 charachters to get file name without extentions
+#remove last 8 charachters (".json.gz") to get file name without extentions
 fileNameRoot=inputFileName[:-8]
 
 #max lines to output in a single csv file
-maxLines=3000000
+linesPerOutputFile=3000000
 
+#initialize the output file number at zero
 fileNumber=0
 
 #count lines and read in field names on first pass
 with gzip.open (inputFileName,'r') as jsonfile:
-    linecounter=0
+    totalLinesInInputFile=0
     t0 = time.time()
     for line in jsonfile:
         lineDictionary=eval(line)
-        linecounter+=1
+        totalLinesInInputFile+=1
         break
-    #fieldnames=[key for key, value in lineDictionary.items()] #random order, let's go back to hard coded
+    #fieldnames=[key for key, value in lineDictionary.items()]
+    #this makes the fields save in random order in the output csv, let's go back to hard coded
     for line in jsonfile:
-        linecounter+=1
+        totalLinesInInputFile+=1
     t1 = time.time()
     #print(str(t1-t0)+" seconds")
-    print(str(linecounter)+" lines to convert to csv.")
+    print(str(totalLinesInInputFile)+" lines to convert to csv.")
 
 fieldnames=['reviewTime',
              'overall',
@@ -44,27 +45,28 @@ fieldnames=['reviewTime',
 #convert to multiple csv files on second pass
 with gzip.open (inputFileName,'r') as jsonfile:
 
-    while 1==1:
-        fileNumber+=1
-        outputFileName=inputFileName+'_output'+str(fileNumber)+'.csv'
-        counter=0
-        totalLines=counter+(fileNumber-1)*maxLines
-        if totalLines<linecounter:
+    while 1 == 1:
+        fileNumber += 1
+        outputFileName = inputFileName + '_output' + str(fileNumber) + '.csv'
+        countOfLinesInThisFile = 0
+        linesDoneSoFar = countOfLinesInThisFile + (fileNumber - 1) * linesPerOutputFile
+        if linesDoneSoFar < totalLinesInInputFile:
             with open(outputFileName, 'w') as csvfile:
-                spamwriter = csv.DictWriter(csvfile,
+                OutputCsvFileWriter = csv.DictWriter(csvfile,
                                             fieldnames=fieldnames)
-                spamwriter.writeheader()
+                OutputCsvFileWriter.writeheader()
 
                 t0 = time.time()
                 for line in jsonfile:
+                    #evaluate the line of json, which will make it a python dictionary.
                     lineDictionary=eval(line)
-                    spamwriter.writerow(lineDictionary)
-                    counter+=1
-                    totalLines=counter+(fileNumber-1)*maxLines
-                    print(str(totalLines)+" lines converted, %0.0f" % (totalLines/linecounter*100) +"% done.", end='\r')
-                    if counter==maxLines:
+                    OutputCsvFileWriter.writerow(lineDictionary)
+                    countOfLinesInThisFile+=1
+                    linesDoneSoFar = countOfLinesInThisFile + (fileNumber - 1) * linesPerOutputFile
+                    print(str(linesDoneSoFar) + " lines converted, %0.0f" % (linesDoneSoFar / totalLinesInInputFile * 100) + "% done.", end='\r')
+                    if countOfLinesInThisFile == linesPerOutputFile:
                         break
                 t1 = time.time()
-                print("File number "+str(fileNumber)+ " took %.2f" % (t1-t0)+ " seconds.                                ")
+                print("File number " + str(fileNumber) + " took %.2f" % (t1-t0) + " seconds.                                ")
         else:
             break
